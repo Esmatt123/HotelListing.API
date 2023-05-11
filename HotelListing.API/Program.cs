72bece02920b0d22fe085a7f5022425bc8b83a41
Esmatt Morra <esmatt98@hotmail.com>
2023-05-11 21:46:28 +0200

    Added Hotels Api Endpoint

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -32,6 +32,7 @@ builder.Services.AddAutoMapper(typeof(MapperConfig));
 
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
+builder.Services.AddScoped<IHotelsRepository, HotelsRepository>();
 
 builder.Services.AddControllers().AddJsonOptions(x =>
 
